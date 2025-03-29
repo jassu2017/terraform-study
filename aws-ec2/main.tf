0b401c0745356d44d3cb8d0e837190b2d5528aaa
Jayshree Prasad <jayshree.prasad@ltimindtree.com>
2025-03-29 11:07:20 +0000

    ec2-medium

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -1,13 +1,13 @@
-# resource "aws_vpc" "my-trfm-aws-vpc" {
-#   cidr_block       = "${var.vpc_cidr_block}"
-#   instance_tenancy = "default"
+resource "aws_vpc" "my-trfm-aws-vpc" {
+  cidr_block       = "${var.vpc_cidr_block}"
+  instance_tenancy = "default"
 
-#   tags = {
-#     name = "my-trfm-aws-vpc"
-#   }
+  tags = {
+    name = "my-trfm-aws-vpc"
+  }
 
 
-# }
+}
 
 
 resource "aws_subnet" "my-trfm-aws-public-subnet" {
@@ -42,10 +42,7 @@ resource "aws_internet_gateway" "my-trfm-aws-igw" {
 }
 
 
-# resource "aws_internet_gateway_attachment" "my-trfm-aws-igw-attach" {
-#   internet_gateway_id = aws_internet_gateway.my-trfm-aws-igw.id
-#   vpc_id              = aws_vpc.my-trfm-aws-vpc.id
-# }
+
 
 resource "aws_route_table" "my-trfm-aws-public-rt" {
   vpc_id = aws_vpc.my-trfm-aws-vpc.id
@@ -145,7 +142,7 @@ resource "aws_key_pair" "demo-key" {
 }
 
 resource "aws_ebs_volume" "ec2-m-ebs-volume" {
-  availability_zone = var.region
+  availability_zone = aws_subnet.my-trfm-aws-public-subnet.availability_zone
   size              = 20
 
   tags = {
@@ -158,89 +155,6 @@ resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.ec2-m-ebs-volume.id
   instance_id = aws_instance.my-trfm-aws-ec2.id
+  force_detach =  true
 }
 
-# resource "aws_cloudwatch_log_group" "my-trfm-aws-grp" {
-#   name = "my-trfm-aws-grp"
-
-#   tags = {
-#     Environment = "dev"
-#     Application = "vpc flow log"
-#   }
-# }
-
-# resource "aws_iam_role" "flow_log_role" {
-#   name = "flow_log_iam_role"
-
-#   # Terraform's "jsonencode" function converts a
-#   # Terraform expression result to valid JSON syntax.
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "vpc-flow-logs.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-  
-#   ]
-
-# }
-# EOF
-
-#   tags = {
-#     tag-key = "flow_log_role"
-#   }
-# }
-
-
-# resource "aws_iam_policy" "flow_log_cloudwatch_log_policy" {
-#   name = "flow_log_cloudwatch_log_iam_policy"
-#   description =  "Policy to allowcloudwatch log operation"
-
-
-#   # Terraform's "jsonencode" function converts a
-#   # Terraform expression result to valid JSON syntax.
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = [
-#           "logs:Creategroup",
-#           "logs:CreateLogstream",
-#           "logs:PutLogEvents",
-#           "logs:DescribeLogGroups",
-#           "logs:DescribeLogStreams",
-
-#         ]
-#         Effect   = "Allow"
-#         Resource = "*"
-#       },
-#     ]
-#   })
-# }
-
-
-#  resource "aws_iam_policy_attachment" "cloudwatch_policy" {
-#    name       = "cloudwatch-policy"
-#    roles      = [aws_iam_role.flow_log_role.name]
-#    # policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
-#    policy_arn = aws_iam_policy.flow_log_cloudwatch_log_policy.arn
-#  }
-
-# #resource "aws_iam_role_policy" "my-trfm-aws-iam-role-policy" {
-# #  name   = "my-trfm-aws-iam-role-policy"
-# #  role   = aws_iam_role.flow_log_role.id
-# #  policy = aws_iam_policy.flow_log_cloudwatch_log_policy.id
-# #}
-
-# resource "aws_flow_log" "demo-flow-log" {
-#   iam_role_arn    = aws_iam_role.flow_log_role.arn
-#   max_aggregation_interval = 60
-#   log_destination = aws_cloudwatch_log_group.my-trfm-aws-grp.arn
-#   traffic_type    = "ALL"
-#   vpc_id          = aws_vpc.my-trfm-aws-vpc.id
-# }
