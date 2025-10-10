797d84af53f3b44923fe667627aff0d16ee36f03
Jayshree Prasad <jayshree.prasad@live.com>
2025-10-10 05:56:03 +0000

    added default

--- s3-datasrc/input.tf
+++ s3-datasrc/input.tf
@@ -2,6 +2,7 @@ variable "region" {
 
   description = "AWS region for s3"
   type = string
+  default = "ap-south-1"
   
 }
 
@@ -9,11 +10,13 @@ variable "bucket_name"{
 
   description = "name of the for s3"
   type = string
+  default = "demo-bkt-101025"
 
 }
 
 variable "s3_bucket_key_name" {
   description = "name of the for s3 object key"
   type = string
+  default = "demo-bkt-101025-key"
  
 }
